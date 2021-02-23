41b8c2d5aa2ac3fc8828250ccf4e2c8f3b0c0c6c
Ryan Overton <ryan.overton@outlook.com>
2021-02-23 11:55:17 -0600

    Cleanup KontentUpdate AZ function

--- api/CodeWithSeanSiteSearchFunction/KontentUpdate.cs
+++ api/CodeWithSeanSiteSearchFunction/KontentUpdate.cs
@@ -27,8 +27,7 @@ namespace CodeWithSeanSiteSearchFunction
         public async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
-        {
-            
+        {            
             log.LogInformation("C# HTTP trigger function processed a request.");
             var body = JsonConvert.SerializeObject(
                 new {
@@ -37,36 +36,19 @@ namespace CodeWithSeanSiteSearchFunction
                         unit = false, 
                         integration = true 
                         }
-                    });
-            // const body = {
-            //     "event_type": "kontent_publish",
-            //     "client_payload": {
-            //         "unit": false,
-            //         "integration": true
-            //     }
-            // }
-            // const headers = {
-            //     "Authorization": `Bearer ${process.env.GITHUB_TOKEN}`,     
-            //     "Accept":"application/vnd.github.everest-preview+json",
-            //     "Content-Type":"application/json"
-            // }     
-            
+                    });            
 
             httpClient.DefaultRequestHeaders.Authorization = 
                 new AuthenticationHeaderValue("Bearer", configuration["GITHUB_TOKEN"]);
             httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/vnd.github.everest-preview+json"));
             httpClient.DefaultRequestHeaders.Add("User-Agent", "request");
-            var url = "https://api.github.com/repos/DevsGarage/seans-new-site/dispatches";
+
+            var url = configuration["GITHUB_DISPATCH_URL"];
             var content = new StringContent(body, System.Text.Encoding.UTF8, "application/json");
             var response = await httpClient.PostAsync(url, content);
             var result = await response.Content.ReadAsStringAsync();
+            log.LogInformation(result);
             return new StatusCodeResult((int)response.StatusCode);
-            // try{ 
-            //     const result = await axios.post("https://api.github.com/repos/OnyxPrime/onyxprime.github.io/dispatches", body, {headers})        
-            //     return { statusCode: result.status, body: result.data }
-            // } catch(Exception e){
-                
-            // }
         }
     }
 }
